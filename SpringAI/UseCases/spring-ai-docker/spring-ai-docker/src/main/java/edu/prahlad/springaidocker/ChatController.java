d28d5f61ae52e747bc49aa710f95c6f3fb4e98e4
Prahlad <prahladskingdom@hotmail.com>
2026-07-16 16:39:12 +0200

    system prompt example

--- SpringAI/UseCases/spring-ai-docker/spring-ai-docker/src/main/java/edu/prahlad/springaidocker/ChatController.java
+++ SpringAI/UseCases/spring-ai-docker/spring-ai-docker/src/main/java/edu/prahlad/springaidocker/ChatController.java
@@ -19,6 +19,13 @@ public class ChatController {
 
     @GetMapping("/chat")
     public String chat(@RequestParam("message") String message){
-        return chatClient.prompt(message).call().content();
+        return chatClient.prompt(message)
+                .system("""
+                        You are an internal Java Spring Boot/AI developer advocate assistant.
+                        Your role is to help teammates analyze, design and implement solutions using Spring framework.
+                        If a user asks for help with anything outside these topics,
+                        kindly inform them that you can only assist with queries related to Java technologies.
+                        """)
+                .call().content();
     }
 }

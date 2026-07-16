c789495debe0a20cd8d032aa52657f1819fd65a9
Prahlad <prahladskingdom@hotmail.com>
2026-07-16 15:45:20 +0200

    Spring AI with Docker initial setup

---
+++ SpringAI/UseCases/spring-ai-docker/spring-ai-docker/src/test/java/edu/prahlad/springaidocker/SpringAIApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.prahlad.springaidocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAIApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
